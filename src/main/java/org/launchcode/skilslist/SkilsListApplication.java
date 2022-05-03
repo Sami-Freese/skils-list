5aca3d4ff191ba6dc567dfa9c4c38bbc885a0307
Sami Freese <sami.freese@gmail.com>
2022-05-02 19:00:16 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/skilslist/SkilsListApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.skilslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilsListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/skilslist/SkilsListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.skilslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
